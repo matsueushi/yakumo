0a1d291e9973e60ac433e910a5c8c411a5addbf1
matsueushi <matsueushi@gmail.com>
2022-10-01 01:01:31 +0000

    permutation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod factor;
 pub mod math;
+pub mod permutation;
 pub mod prime;
---
+++ src/permutation.rs
@@ -0,0 +1,42 @@
+pub fn next_permutation<T: PartialOrd>(nums: &mut [T]) -> bool {
+    let last_asc = match nums.windows(2).rposition(|w| w[0] < w[1]) {
+        None => {
+            nums.reverse();
+            return false;
+        }
+        Some(i) => i,
+    };
+    match nums[last_asc + 1..]
+        .iter()
+        .rposition(|x| x > &nums[last_asc])
+    {
+        None => return false,
+        Some(i) => {
+            nums.swap(last_asc, last_asc + 1 + i);
+            nums[last_asc + 1..].reverse();
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::permutation::*;
+
+    #[test]
+    fn test_next_permutation() {
+        let mut nums = vec![1, 2, 3];
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![1, 3, 2]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![2, 1, 3]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![2, 3, 1]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![3, 1, 2]);
+        assert!(next_permutation(&mut nums));
+        assert_eq!(nums, vec![3, 2, 1]);
+        assert!(!next_permutation(&mut nums));
+        assert_eq!(nums, vec![1, 2, 3]);
+    }
+}
