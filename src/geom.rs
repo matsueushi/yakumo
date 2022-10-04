766ff824600514b44cfa5d21c0972b93fe63569f
matsueushi <matsueushi@gmail.com>
2022-10-04 14:57:46 +0000

    geom

--- src/geom.rs
+++ src/geom.rs
@@ -11,12 +11,16 @@ pub struct Point<T> {
 macro_rules! point_impl {
     ($($t:ty)*) => ($(
         impl Point<$t> {
+            pub fn new(x: $t, y:$t) -> Self {
+                Self { x, y }
+            }
+
             /// 二点を通る直線を構成する。
             pub fn line(&self, other: &Self) -> Line<$t> {
                 let (x0, y0) = (self.x, self.y);
                 let (x1, y1) = (other.x, other.y);
-                let a = x1 - x0;
-                let b = -y1 + y0;
+                let a = y1 - y0;
+                let b = -x1 + x0;
                 let c = x0 * y1 - x1 * y0;
                 Line { a, b, c }
             }
@@ -47,10 +51,22 @@ macro_rules! line_impl {
                     c = -c;
                 }
                 let g = a.gcd(&b);
-                Self {a:a/g,b:b/g,c:c/g}
+                Self { a: a / g, b: b / g, c: c / g}
             }
         }
     )*)
 }
 
 line_impl! { isize i8 i16 i32 i64 i128 }
+
+#[cfg(test)]
+mod tests {
+    use crate::geom::{Line, Point};
+
+    #[test]
+    fn test_point() {
+        let p0 = Point::<i64>::new(0, 0);
+        let p1 = Point::<i64>::new(1, 2);
+        assert_eq!(Line { a: 2, b: -1, c: 0 }, p0.line(&p1));
+    }
+}
